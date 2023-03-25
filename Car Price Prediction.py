f9fe22f1789c756c07938c4889e09a6907349154
Mijul Saxena <115783136+MijulSaxena@users.noreply.github.com>
2023-03-25 16:39:16 +0530

    Add files via upload

--- Car Price Prediction.py
+++ Car Price Prediction.py
@@ -16,7 +16,7 @@
 # 8. Transmission
 # 9. Owner
 
-# In[18]:
+# In[1]:
 
 
 import pandas as pd
@@ -28,38 +28,54 @@ from sklearn.linear_model import Lasso
 from sklearn import metrics
 
 
-# In[19]:
+# In[2]:
 
 
 # loading the csv file 
 df = pd.read_csv('Car data.csv')
 
 
-# In[20]:
+# In[3]:
 
 
 df.head(10)
 
 
-# In[21]:
+# In[4]:
 
 
 df.shape
 
 
-# In[22]:
+# In[5]:
 
 
 df.info()
 
 
-# In[23]:
+# In[6]:
 
 
 df.describe()
 
 
-# In[25]:
+# In[7]:
+
+
+plt.figure(figsize = (10,7))
+sns.heatmap(df.corr(), annot = True)
+plt.title('Correlation between colums')
+
+
+# In[8]:
+
+
+fig = plt.figure(figsize = (10, 5))
+plt.title('Correlation between present price and selling price')
+sns.regplot(x = 'Present_Price', y = 'Selling_Price', data = df)
+
+
+# In[9]:
 
 
 # checking the distribution of categorical data
@@ -70,7 +86,7 @@ print(df['Transmission'].value_counts())
 
 # **Encoding the Categorical Data**
 
-# In[26]:
+# In[10]:
 
 
 # encoding "Fuel_Type" Column
@@ -83,16 +99,109 @@ df.replace({'Seller_Type':{'Dealer':0,'Individual':1}},inplace=True)
 df.replace({'Transmission':{'Manual':0,'Automatic':1}},inplace=True)
 
 
-# In[27]:
+# In[11]:
 
 
 df.head()
 
 
+# **Splitting the data**
+
+# In[12]:
+
+
+X =  df.drop(['Car_Name', 'Selling_Price'], axis = 1)
+Y = df['Selling_Price']
+
+
+# In[13]:
+
+
+X
+
+
+# In[14]:
+
+
+Y
+
+
 # **Splitting the data into Train and Test**
 
-# In[ ]:
+# In[15]:
 
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 2)
 
+
+# **Training Our Model**
+
+# In[16]:
+
+
+LR_model = LinearRegression()
+
+
+# In[17]:
+
+
+LR_model.fit(X_train, Y_train)
+
+
+# **Model Evaluation**
+
+# In[18]:
+
+
+# prediction on training data
+train_data_prediction = LR_model.predict(X_train)
+
+
+# In[19]:
+
+
+# Comparing predicted and target value by R square Error
+error = metrics.r2_score(Y_train, train_data_prediction)
+
+
+# In[20]:
+
+
+error
+
+
+# In[21]:
+
+
+plt.scatter(Y_train, train_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+
+
+# In[22]:
+
+
+# prediction on test data
+test_data_prediction = LR_model.predict(X_test)
+
+
+# In[23]:
+
+
+# Comparing predicted and target value by R square Error
+error = metrics.r2_score(Y_test, test_data_prediction)
+
+
+# In[24]:
+
+
+error
+
+
+# In[25]:
+
+
+plt.scatter(Y_test, test_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
 
